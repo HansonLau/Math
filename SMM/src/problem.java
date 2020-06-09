0a80f88792677e576613d7876d25865bbb6a76cf
Hanson <shadowguy369@gmail.com>
2020-06-09 11:40:04 -0700

    First commit

---
+++ SMM/src/problem.java
@@ -0,0 +1,27 @@
+
+public class problem {
+	
+	private String type;
+	private int amt;
+	public problem(String type, int amt){
+		
+		this.type = type;
+		this.amt = amt;
+		
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getAmt() {
+		return amt;
+	}
+	public void setAmt(int amt) {
+		this.amt = amt;
+	}
+	
+
+
+}
