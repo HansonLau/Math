bd4e709e72abe2eec2f87027ded4820aaaadbb87
Hanson <shadowguy369@gmail.com>
2020-06-11 19:03:02 -0700

    added timing

--- Timing/src/practice2.java
+++ Timing/src/practice2.java
@@ -1,6 +1,7 @@
 import java.awt.Component;
 import javax.swing.JOptionPane;
 import java.util.Arrays;
+import java.util.Date;
 
 //https://github.com/HansonLau/SMM.git
 
@@ -10,6 +11,8 @@ public class practice2 {
 	private int amt;
 	private double correct = 0; 
 	private double[] user;
+	long startTime = System.currentTimeMillis();
+	long elapsedTime = 0L;
 	
 	public practice2() {
 
@@ -37,25 +40,30 @@ public class practice2 {
 		questions2 q = new questions2(type, amt);
 		user = new double[amt];
 		//shows questions
-		
-		int i = 0;
-		for(problem2 temp: q.getQuestion()) {
-			
-			if(type.equals("divide")) {
-				double input2 = Double.parseDouble(JOptionPane.showInputDialog(temp.getProblem()));
-				if(input2 == temp.getAnswer())
-					correct++;
-				user[i] = input2;
-			}
+		long startTime = System.currentTimeMillis();
+		double elapsedTime = 0;
+		while (elapsedTime < 2*60*1000) {
+			int i = 0;
+			for(problem2 temp: q.getQuestion()) {
 				
-			else {
-				int input = Integer.parseInt(JOptionPane.showInputDialog(temp.getProblem()));
-				if(input == temp.getAnswer())
-					correct++;
-				user[i] = input;
+				if(type.equals("divide")) {
+					double input2 = Double.parseDouble(JOptionPane.showInputDialog(temp.getProblem()));
+					if(input2 == temp.getAnswer())
+						correct++;
+					user[i] = input2;
+				}
+					
+				else {
+					int input = Integer.parseInt(JOptionPane.showInputDialog("Time : " + elapsedTime +" " +temp.getProblem()));
+					if(input == temp.getAnswer())
+						correct++;
+					user[i] = input;
+				}
+				i++;
+				elapsedTime = (new Date()).getTime() - startTime;
+				System.out.println(elapsedTime + "milli seconds");
 			}
-			
-			i++;
+			break;
 		}
 		
 		
@@ -64,7 +72,10 @@ public class practice2 {
 		JOptionPane.showMessageDialog(null, "You got " + percent + "% correct.\n"
 				+ "Your answers: " + Arrays.toString(user)
 				+ "\nAnswers: " + Arrays.toString(q.getKey()));
+		elapsedTime = (int)elapsedTime/10; 
+		elapsedTime = elapsedTime/100; // rounding to one decimal
 		
+		JOptionPane.showMessageDialog(null, "It took " + elapsedTime + " seconds."); // add time
 	}
 	
 	private Component typ() {
